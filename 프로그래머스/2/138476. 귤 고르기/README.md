6f4fb6a181c9671b51221d59e9c4950371e06bb2
rudtnrdid123 <151603324+rudtnrdid123@users.noreply.github.com>
2024-07-24 23:01:48 +0900

    [level 2] Title: 귤 고르기, Time: 104.55 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 108 MB, 시간: 146.32 ms
+메모리: 93.2 MB, 시간: 104.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 24일 23:00:32
+2024년 07월 24일 23:01:45
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,29 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        
-        for(int i = 0; i< tangerine.length; i++){
-            map.put(tangerine[i],map.getOrDefault(tangerine[i],0)+1);
+        HashMap<Integer,Integer> map =new HashMap<>();
+
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
-        
-        List<Map.Entry<Integer,Integer>> entryMap = new ArrayList<>(map.entrySet());
-        Collections.sort(entryMap, (o1,o2) -> o2.getValue() - o1.getValue());
-        
-        int sum = 0;
-        for(Map.Entry<Integer,Integer> entry : entryMap){
-            int value = entry.getValue();
-            if(k <= value)
-                return 1;
-            
-            sum += value;
+
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2)-map.get(o1));
+
+        for(Integer key:list){
+            k -=map.get(key);
             answer++;
-            
-            if(sum >= k)
+            if(k<=0){
                 break;
+            }
         }
-        
+
         return answer;
     }
 }
