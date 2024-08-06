c0db46e40a2dbbd772ae21fd62c9798f1ef8aa22
rudtnrdid123 <151603324+rudtnrdid123@users.noreply.github.com>
2024-08-06 23:44:15 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -1,32 +1,30 @@
-import java.util.*;
 import java.io.*;
 
 class Main {
-    public static String answer;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         
-        answer = br.readLine();
-        for(int i = 0; i<n-1; i++){
+        String answer = br.readLine();
+        StringBuilder result = new StringBuilder(answer);
+
+        for (int i = 1; i < n; i++) {
             String str = br.readLine();
-            compare(answer,str);
+            compare(result, answer, str);
+            answer = result.toString(); // 업데이트된 answer로 갱신
         }
-        sb.append(answer);
-        System.out.println(sb.toString());
+
+        System.out.println(answer);
     }
 
-    public static void compare(String s1, String s2){
-        String[] arr1 = s1.split("");
-        String[] arr2 = s2.split("");
-        answer = "";
-        
-        for(int i = 0; i<arr1.length; i++){
-            if(arr1[i].equals(arr2[i]))
-                answer += arr1[i];
-            else
-                answer += "?";
+    public static void compare(StringBuilder result, String s1, String s2) {
+        result.setLength(0); // StringBuilder 초기화
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
+                result.append(s1.charAt(i));
+            } else {
+                result.append('?');
+            }
         }
     }
 }
