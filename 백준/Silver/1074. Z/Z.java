8bca51c6101a212020c40620dd439100d5e5eb6a
rudtnrdid123 <151603324+rudtnrdid123@users.noreply.github.com>
2024-08-11 00:27:20 +0900

    [Silver I] Title: Z, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 class Main {
     public static int cnt = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,33 +12,28 @@ class Main {
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        int line = (int)Math.pow(2,N);
-        Find(line,r,c);
+        int line = 1 << N;
+        Find(line, r, c);
         System.out.println(cnt);
     }
 
-    public static void Find(int line, int r, int c){
-        int halfLine = line/2;
-        int firstSu = (int)Math.pow(line,2)/4;
+    public static void Find(int line, int r, int c) {
+        if (line == 1) return;
 
-        if(line == 1)
-            return;
-        
-        if(r < halfLine && c < halfLine){
-            cnt += 0;
-            Find(line/2, r, c);
-        }
-        else if(r < halfLine && c >= halfLine){
+        int halfLine = line / 2;
+        int firstSu = halfLine * halfLine;
+
+        if (r < halfLine && c < halfLine) {
+            Find(halfLine, r, c);
+        } else if (r < halfLine) {
             cnt += firstSu;
-            Find(line/2, r, c-line/2);
-        }
-        else if(r >= halfLine && c < halfLine){
-            cnt += firstSu*2;
-            Find(line/2, r-line/2, c);
-        }
-        else{
-            cnt += firstSu*3;
-            Find(line/2, r-line/2, c-line/2);
+            Find(halfLine, r, c - halfLine);
+        } else if (c < halfLine) {
+            cnt += firstSu * 2;
+            Find(halfLine, r - halfLine, c);
+        } else {
+            cnt += firstSu * 3;
+            Find(halfLine, r - halfLine, c - halfLine);
         }
     }
 }
